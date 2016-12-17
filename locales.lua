4874c8d8b9b9cdd6f0ac186a3eafca130dbf1a84
KwirkyJ <kwirkyj.smith0@gmail.com>
2016-12-17 16:58:38 -0600

    add tostring() metamethod and modify add_chunk(chunk) for quicker string generation

--- locales.lua
+++ locales.lua
@@ -4,28 +4,76 @@
 local locales_has, locales_get_count
 local locales_add_chunk, locales_get_random_chunk
 
+local function locales_tostr (L)
+    local t = {}
+    for i=1, L._n do
+        local chunk = L._flat[i]
+        if i ==1 then
+            t[i] = "("..chunk.x..", "..chunk.y..")"
+        else
+            t[i] = ", ("..chunk.x..", "..chunk.y..")"
+        end
+    end
+    return table.concat (t)
+end
+
 ---Create new locales "object" 
 ---@return Locales table-object-structure
 local function init_locales ()
-    return {_n = 0, 
-            _flat = {},
-            add_chunk = locales_add_chunk,
-            get_random_chunk = locales_get_random_chunk,
-            get_count = locales_get_count,
-            has = locales_has,
-           }
+    local L = {_n = 0, 
+               _flat = {},
+               add_chunk = locales_add_chunk,
+               get_random_chunk = locales_get_random_chunk,
+               get_count = locales_get_count,
+               has = locales_has,
+              }
+    setmetatable (L, {__tostring = locales_tostr})
+    return L
+end
+
+---Create a new table where the chunks are sorted, least-x, least-y first
+---Is expensive at present, O(x^2), but should not be called frequently
+---@param T table of Chunks ({{x=n, y=m}}, e.g.)
+---@return Table
+local function sort_chunks (T)
+    local sorted = {}
+    local chunk, added
+    for _=1, #T do
+        added, chunk = false, table.remove (T)
+        for j=1, #sorted do
+            if sorted[j] and
+               sorted[j].x > chunk.x 
+            then
+                table.insert (sorted, j, chunk)
+                added = true
+                break
+            elseif sorted[j] and
+                   sorted[j].x == chunk.x and 
+                   sorted[j].y > chunk.y 
+            then
+                table.insert (sorted, j, chunk)
+                added = true
+                break
+            end
+        end
+        if not added then
+            table.insert (sorted, chunk)
+        end
+    end
+    return sorted
 end
 
 ---Add a Chunk to the structure
 ---@param chunk Chunk ({x=0, y=32}, e.g.)
 locales_add_chunk = function (self, chunk)
-    local row = self[chunk.x]
+    local row, x, y = self[chunk.x], chunk.x, chunk.y
     if row then
-        row[chunk.y] = true
+        row[y] = true
     else
-        self[chunk.x] = {[chunk.y] = true}
+        self[x] = {[y] = true}
     end
-    table.insert (self._flat, {x = chunk.x, y = chunk.y})
+    table.insert (self._flat, {x = x, y = y})
+    self._flat = sort_chunks (self._flat)
     self._n = self._n + 1
 end
 
--- test_locales.lua
+++ test_locales.lua
@@ -20,12 +20,19 @@ for _=1, 10 do
     assert (locales:has (locales:get_random_chunk ()))
 end
 
+-- test tostring
+assert (tostring (locales) == "(-32, 0), (0, 0), (0, 64), (96, 0)", 
+        tostring(locales))
+
 -- test duplicate addition
 assert (locales:get_count () == 4)
 assert (locales:has ({x=96, y=0}))
 locales:add_chunk ({x=96, y=0})
 assert (locales:get_count () == 5)
 assert (locales:has ({x=96, y=0}))
+
+assert (tostring (locales) == "(-32, 0), (0, 0), (0, 64), (96, 0), (96, 0)", 
+        tostring(locales))
 
 print ("=== TESTS SUCCESSFUL ===")
 
